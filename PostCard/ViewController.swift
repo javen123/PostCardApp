e23ff0b68a903bfab66322cf389e492bee6f4183
javen123 <thisjave@gmail.com>
2014-09-24 17:03:25 -0500

    first commit

---
+++ PostCard/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  PostCard
+//
+//  Created by user on 9/24/14.
+//  Copyright (c) 2014 Neva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var enterNameTextField: UITextField!
+    
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    
+    @IBOutlet weak var mailButtonSent: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        
+        messageLabel.hidden = false
+        messageLabel.text = enterMessageTextField.text
+        enterMessageTextField.text = ""
+        enterMessageTextField.resignFirstResponder()
+        messageLabel.textColor = UIColor.redColor()
+        mailButtonSent.setTitle("Mail Sent", forState:UIControlState.Normal)
+    }
+
+}
+
